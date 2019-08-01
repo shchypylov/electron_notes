1959b7a7eadb0e8b332ee0916a9b1d54560501a9
Nikita Shchypylov <shchypylov@gmail.com>
2019-08-02 00:16:50 +0300

    search improvments

--- src/containers/Search.js
+++ src/containers/Search.js
@@ -4,31 +4,45 @@ import { Post as PostComponent } from '../components/Post'
 import { Comment } from '../components/Comment'
 
 class Search extends Component {
-    render() {
+    filterItems = type => {
         const { items } = this.props
+
+        return type === 'posts' ? items.filter(item => item.userId) : items.filter(item => item.postId)
+    }
+
+    render() {
+        const posts = this.filterItems('posts'),
+            comments = this.filterItems('comments')
+
         return (
             <div>
-                <h3>Results:</h3>
-                {items.map(item => {
-                    const key = item.userId ? `${item.userId}_${item.id}` : `${item.postId}_${item.id}`
+                {posts.length > 0 || comments.length > 0 ? <h1>Results:</h1> : <h1>No results :(</h1>}
 
-                    return (
-                        <div key={key}>
-                            {item.userId ? (
-                                <PostComponent
-                                    title={item.title}
-                                    body={item.body}
-                                />
-                            ) : (
-                                <Comment
-                                    title={item.name}
-                                    body={item.body}
-                                    email={item.email}
-                                />
-                            )}
-                        </div>
-                    )
-                })}
+                {posts.length > 0 && (
+                    <>
+                        <h2>Posts:</h2>
+                        {posts.map(item => {
+                            return (
+                                <div key={`${item.userId}_${item.id}`}>
+                                    <PostComponent title={item.title} body={item.body} />
+                                </div>
+                            )
+                        })}
+                    </>
+                )}
+
+                {comments.length > 0 && (
+                    <>
+                        <h2>Comments:</h2>
+                        {comments.map(item => {
+                            return (
+                                <div key={`${item.postId}_${item.id}`}>
+                                    <Comment title={item.name} body={item.body} email={item.email} />
+                                </div>
+                            )
+                        })}
+                    </>
+                )}
             </div>
         )
     }
